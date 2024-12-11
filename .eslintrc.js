4a87b6c6137ef24952f9a37f692e60d0d7f653da
Wellington Braga <wellingtonbat.wl@gmail.com>
2024-12-11 01:07:54 -0300

    feat: :wrench: Corrige e sincroniza o eslint com prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,32 @@
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const js = require("@eslint/js");
+
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  ...js.configs.recommended,
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: 'tsconfig.json',
+    project: "tsconfig.json",
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    // 'plugin:prettier/recommended',
-  ],
+  plugins: ["@typescript-eslint/eslint-plugin"],
+  extends: ["plugin:@typescript-eslint/recommended"],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: [".eslintrc.js"],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    "no-console": "warn",
+    "no-unused-vars": "error",
+    "no-undef": "error",
+    "no-empty-function": "warn",
+    "no-unused-expressions": "error",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
   },
+  eslintPluginPrettierRecommended,
 };
