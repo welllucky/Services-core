94473b258f4174e446934d04c527d3be0f8dec46
Wellington Braga <wellingtonbat.wl@gmail.com>
2025-03-16 22:03:35 -0300

    feat: :sparkles: Adiciona suporte para filtragem de tickets pelo resolutor e implementa novos endpoints para iniciar e resolver tickets

--- src/modules/ticket/ticket.service.ts
+++ src/modules/ticket/ticket.service.ts
@@ -3,6 +3,7 @@ import {
   ITicket,
   Pagination,
   PublicTicketDto,
+  SearchedTicketDto,
   UpdateTicketDto,
 } from "@/typing";
 import { getUserByToken } from "@/utils";
@@ -18,64 +19,9 @@ export class TicketService {
     private readonly userModel: UserModel,
   ) {}
 
-  async create(
-    token: string,
-    ticketData: Omit<ITicket, "id" | "createdAt" | "updatedAt" | "closedAt">,
-  ): Promise<IResponseFormat<PublicTicketDto>> {
-    const { userData } = await getUserByToken(token);
-    const userId = userData?.register;
-
-    await this.userModel.init({
-      register: userId,
-    });
-
-    if (!this.userModel.exists()) {
-      throw new HttpException(
-        {
-          title: "Access denied",
-          message: "User cannot create a ticket without being logged in.",
-        },
-        HttpStatus.UNAUTHORIZED,
-      );
-    }
-
-    const ticket = await this.repository.create(
-      ticketData,
-      this.userModel.getEntity(),
-    );
-
-    if (!ticket) {
-      throw new HttpException(
-        {
-          title: "Ticket not created",
-          message: "Occurred an error while creating the ticket",
-        },
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-
-    return {
-      data: new PublicTicketDto(
-        ticket.id,
-        ticket.resume,
-        ticket.description,
-        ticket.date,
-        ticket.priority,
-        ticket.type,
-        ticket.status,
-        ticket.resolver?.register,
-        // ticket.events,
-        ticket?.createdAt,
-        ticket?.closedAt,
-        ticket.createdBy.register,
-        ticket.closedBy?.register,
-      ),
-      message: "Ticket created successfully",
-    };
-  }
-
   async getAll(
     token: string,
+    isSolver?: boolean,
     pagination?: Pagination,
   ): Promise<IResponseFormat<PublicTicketDto[]>> {
     const { userData } = await getUserByToken(token);
@@ -98,16 +44,11 @@ export class TicketService {
     const tickets = await this.repository.findAll({
       userId,
       pagination,
+      isSolver: isSolver,
     });
 
     if (!tickets.length) {
-      throw new HttpException(
-        {
-          title: "Tickets not found",
-          message: "No tickets found for the resolver",
-        },
-        HttpStatus.NO_CONTENT,
-      );
+      throw new HttpException(undefined, HttpStatus.NO_CONTENT);
     }
 
     return {
@@ -121,12 +62,12 @@ export class TicketService {
             ticket.priority,
             ticket.type,
             ticket.status,
-            ticket.resolver?.register,
+            ticket?.resolver?.register,
             // ticket.events,
             ticket.createdAt,
-            ticket.closedAt,
-            ticket.createdBy?.register,
-            ticket.closedBy?.register,
+            ticket?.closedAt,
+            ticket.createdBy.register,
+            ticket?.closedBy?.register,
           ),
       ),
       message: "Tickets found successfully",
@@ -136,6 +77,7 @@ export class TicketService {
   async getById(
     token: string,
     ticketId: string,
+    isSolver?: boolean,
   ): Promise<IResponseFormat<PublicTicketDto>> {
     const { userData } = await getUserByToken(token);
     const userId = userData?.register;
@@ -154,7 +96,7 @@ export class TicketService {
       );
     }
 
-    const ticket = await this.repository.findById(userId, ticketId);
+    const ticket = await this.repository.findById(userId, ticketId, isSolver);
 
     if (!ticket) {
       throw new HttpException("", HttpStatus.NO_CONTENT);
@@ -227,7 +169,6 @@ export class TicketService {
     token: string,
     pagination?: Pagination,
   ): Promise<IResponseFormat<unknown>> {
-    console.log("findInProgress");
     const { userData } = await getUserByToken(token);
     const userId = userData?.register;
 
@@ -274,15 +215,71 @@ export class TicketService {
             ticket.priority,
             ticket.type,
             ticket.status,
-            ticket.resolver.register,
+            ticket?.resolver?.register,
             // ticket.events,
             ticket.createdAt,
-            ticket.closedAt,
-            ticket.createdBy.register,
-            ticket.closedBy.register,
+            ticket?.closedAt,
+            ticket?.createdBy?.register,
+            ticket?.closedBy?.register,
           ),
       ),
       message: `${tickets.length} tickets in progress was founded`,
+    };
+  }
+
+  async create(
+    token: string,
+    ticketData: Omit<ITicket, "id" | "createdAt" | "updatedAt" | "closedAt">,
+  ): Promise<IResponseFormat<PublicTicketDto>> {
+    const { userData } = await getUserByToken(token);
+    const userId = userData?.register;
+
+    await this.userModel.init({
+      register: userId,
+    });
+
+    if (!this.userModel.exists()) {
+      throw new HttpException(
+        {
+          title: "Access denied",
+          message: "User cannot create a ticket without being logged in.",
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    const ticket = await this.repository.create(
+      ticketData,
+      this.userModel.getEntity(),
+    );
+
+    if (!ticket) {
+      throw new HttpException(
+        {
+          title: "Ticket not created",
+          message: "Occurred an error while creating the ticket",
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return {
+      data: new PublicTicketDto(
+        ticket.id,
+        ticket.resume,
+        ticket.description,
+        ticket.date,
+        ticket.priority,
+        ticket.type,
+        ticket.status,
+        ticket.resolver?.register,
+        // ticket.events,
+        ticket?.createdAt,
+        ticket?.closedAt,
+        ticket.createdBy.register,
+        ticket.closedBy?.register,
+      ),
+      message: "Ticket created successfully",
     };
   }
 
@@ -307,7 +304,7 @@ export class TicketService {
       );
     }
 
-    const startedTicket = await this.repository.update(
+    const { isUpdated } = await this.repository.update(
       {
         status: "inProgress",
         updatedAt: new Date(),
@@ -316,7 +313,7 @@ export class TicketService {
       userId,
     );
 
-    if (!startedTicket) {
+    if (!isUpdated) {
       throw new HttpException(
         {
           title: "Ticket not started",
@@ -329,6 +326,49 @@ export class TicketService {
     return {
       title: "Ticket started",
       message: `The ticket ${ticketId} was started successfully`,
+    };
+  }
+
+  async close(
+    token: string,
+    ticketId: string,
+  ): Promise<IResponseFormat<unknown>> {
+    const { userData } = await getUserByToken(token);
+    const userId = userData?.register;
+
+    await this.userModel.init({
+      register: userId,
+    });
+
+    if (!this.userModel.exists()) {
+      throw new HttpException(
+        {
+          title: "Access denied",
+          message: "The ticket cannot be closed without being logged in.",
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    const { isUpdated } = await this.repository.update(
+      {
+        status: "closed",
+      },
+      ticketId,
+      userId,
+      true,
+    );
+    if (!isUpdated) {
+      throw new HttpException(
+        {
+          title: "Ticket not closed",
+          message: "Occurred an error while closing the ticket",
+        },
+        HttpStatus.NOT_MODIFIED,
+      );
+    }
+    return {
+      title: "Ticket closed",
+      message: `The ticket ${ticketId} was closed successfully. The resolver will be notified!`,
     };
   }
 
@@ -353,60 +393,16 @@ export class TicketService {
       );
     }
 
-    const updatedTicket = await this.repository.updateByResolver(
+    const { isUpdated } = await this.repository.update(
       {
         status: "closed",
       },
       ticketId,
       userId,
+      true,
     );
 
-    if (!updatedTicket) {
-      throw new HttpException(
-        {
-          title: "Ticket not resolved",
-          message: "Occurred an error while resolving the ticket",
-        },
-        HttpStatus.NOT_MODIFIED,
-      );
-    }
-
-    return {
-      title: "Ticket resolved",
-      message: `The ticket ${ticketId} was resolved successfully. The resolver will be notified!`,
-    };
-  }
-
-  async resolveByResolver(
-    token: string,
-    ticketId: string,
-  ): Promise<IResponseFormat<unknown>> {
-    const { userData } = await getUserByToken(token);
-    const userId = userData?.register;
-
-    await this.userModel.init({
-      register: userId,
-    });
-
-    if (!this.userModel.exists()) {
-      throw new HttpException(
-        {
-          title: "Access denied",
-          message: "The ticket cannot be resolved without being logged in.",
-        },
-        HttpStatus.UNAUTHORIZED,
-      );
-    }
-
-    const updatedTicket = await this.repository.updateByResolver(
-      {
-        status: "closed",
-      },
-      ticketId,
-      userId,
-    );
-
-    if (!updatedTicket) {
+    if (!isUpdated) {
       throw new HttpException(
         {
           title: "Ticket not resolved",
@@ -419,48 +415,6 @@ export class TicketService {
     return {
       title: "Ticket resolved",
       message: `The ticket ${ticketId} was resolved successfully. The user will be notified!`,
-    };
-  }
-
-  async close(
-    token: string,
-    ticketId: string,
-  ): Promise<IResponseFormat<unknown>> {
-    const { userData } = await getUserByToken(token);
-    const userId = userData?.register;
-
-    await this.userModel.init({
-      register: userId,
-    });
-
-    if (!this.userModel.exists()) {
-      throw new HttpException(
-        {
-          title: "Access denied",
-          message: "The ticket cannot be closed without being logged in.",
-        },
-        HttpStatus.UNAUTHORIZED,
-      );
-    }
-    const updatedTicket = await this.repository.updateByResolver(
-      {
-        status: "closed",
-      },
-      ticketId,
-      userId,
-    );
-    if (!updatedTicket) {
-      throw new HttpException(
-        {
-          title: "Ticket not closed",
-          message: "Occurred an error while closing the ticket",
-        },
-        HttpStatus.NOT_MODIFIED,
-      );
-    }
-    return {
-      title: "Ticket closed",
-      message: `The ticket ${ticketId} was closed successfully. The resolver will be notified!`,
     };
   }
 
@@ -496,7 +450,20 @@ export class TicketService {
     }
 
     return {
-      data: tickets,
+      data: tickets.map(
+        (ticket) =>
+          new SearchedTicketDto(
+            ticket.id,
+            ticket.resume,
+            ticket.description,
+            ticket.date,
+            ticket.priority,
+            ticket.type,
+            ticket.status,
+            ticket.createdBy?.register,
+            ticket?.resolver?.register,
+          ),
+      ),
       message: `${tickets.length} tickets found with the term: ${term}.`,
     };
   }
