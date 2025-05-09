2c87c8ec1582b70fd768ea7ead6f49b9a5386ce5
Wellington Braga <wellingtonbat.wl@gmail.com>
2025-05-09 04:00:35 -0300

    feat: :sparkles: Implementa logging na chamadas feitas para a aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import "./instrument.js";
 import { ConfigService } from "@nestjs/config";
 import { useContainer } from "class-validator";
 import * as express from "express";
 import { join } from "path";
 import { AppModule } from "./app.module.js";
+import "./instrument.js";
 import { configureCors, createAppInstance, enableGlobalPipes, enableVersioning } from "./utils/functions";
 
 async function startTheService() {
@@ -29,9 +29,6 @@ async function startTheService() {
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   await app.listen(configService.get("PORT") ?? 4000);
-
-  // eslint-disable-next-line no-console
-  console.log(`Server is running on: ${await app.getUrl()}`);
 }
 
 startTheService();
