41c4bf539e5f1893d60df05a72597028fad4e9bd
Wellington Braga <wellingtonbat.wl@gmail.com>
2025-05-09 06:20:35 -0300

    feat: :sparkles: Implementa o rate limit na aplicação

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,11 +2,13 @@ import { Controller, Get, Res, VERSION_NEUTRAL } from "@nestjs/common";
 import { Response } from "express";
 import { join } from "path";
 import { IsPublic } from "./utils/decorators/IsPublic.decorator";
+import { Throttle } from "@nestjs/throttler";
 
 @Controller({
     version: VERSION_NEUTRAL,
 })
 export class AppController {
+    @Throttle({ default: { limit: 20, ttl: 60000 } })
     @Get()
     @IsPublic()
     getHtml(@Res() res: Response) {
